0478597786c64ab8c8e0c850215d4d386584bd71
mattlaguardia <matthewlaguardia@gmail.com>
2018-03-08 09:54:42 -0800

    feat(html emails): adds functionality to email.php"

--- dist/functions/email.php
+++ dist/functions/email.php
@@ -18,10 +18,15 @@ function rg_serve_route () {
   $from = 'info@wordpress.com';
   $to = 'matt@roostergrin.com';
   $subject = 'API Contact Form';
-  $message = 'Form Submission by: ' . $data['firstname'] . ' ' . $data['lastname'] . "\r\n";
-  $message .= 'Email: ' . $data['email'] . "\r\n";
-  $message .= 'Phone: ' . $data['phone'] . "\r\n";
-  $headers = '';
+  // $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
+  // $headers .= "Reply-To: " . "\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+  $message = '<html><body>';
+  $message .= '<p>Form Submission by: ' . $data['firstname'] . ' ' . $data['lastname'] . '</p>';
+  $message .= '<p>Email: ' . $data['email'] . '</p>';
+  $message .= '<p>Phone: ' . $data['phone'] . '</p>';
+  $message .= '</body></html>';
 
   $sent_message = wp_mail($to, $subject, $message, $headers);
 
