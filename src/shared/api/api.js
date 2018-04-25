4f6a8b030a657fc66490a553c7ff703993c68f0b
spragala <me@spragala.com>
2018-04-25 11:17:00 -0700

    initial commit

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -1,4 +1,4 @@
-const route = '//api.roostertest3.com/wp-json'
+const route = '//mattiacioortho.roostertest3.com/wp-json'
 
 const api = () => {
   return document.location.hostname === 'localhost' || document.location.href.indexOf('roostertest3') > -1 ? route : document.location.protocol + '/wp-json'
