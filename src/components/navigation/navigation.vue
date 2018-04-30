04bcb4a8b1a3bc4d198296fd2e6fc677a8c949f8
spragala <me@spragala.com>
2018-04-30 11:50:11 -0700

    WIP menu / added nav components

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -1,1 +1,15 @@
 <template lang='pug' src='./navigation.pug'></template>
+
+<script>
+import Desktop from './nav-desktop/nav-desktop'
+import Mobile from './nav-mobile/nav-mobile'
+
+export default {
+  props: ['props'],
+  components: {
+    Desktop,
+    Mobile
+  }
+}
+
+</script>
