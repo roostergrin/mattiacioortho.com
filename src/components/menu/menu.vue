fe4cf7a30cad99ea368d3a19de31b4b8e35fbbd5
spragala <me@spragala.com>
2018-05-01 16:54:24 -0700

    WIP menu - responsive thumbnails open correctly

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -3,6 +3,12 @@
 <script>
 
 export default {
+  data () {
+    return {
+      styleWidth: window.innerWidth,
+      styleHeight: window.innerHeight
+    }
+  },
   computed: {
     links () {
       return this.$router.options.routes
@@ -11,16 +17,26 @@ export default {
       return {
         'active': this.$store.state.menu
       }
+    },
+    styleObject () {
+      return {
+        width: (this.styleWidth / 4) + 'px',
+        height: (this.styleHeight / 4) + 'px'
+      }
     }
   },
-  created () {
-    console.log(this.$route)
+  mounted () {
+    console.log(this.$router.options.routes.url)
+    window.addEventListener('resize', this.myEventHandler)
   },
   methods: {
     closeMenu (e) {
-      console.log(e)
       this.$store.state.menu ? this.$store.dispatch('VIEW_MENU', false) : this.$store.dispatch('VIEW_MENU', true)
       this.$router.push(e)
+    },
+    myEventHandler (e) {
+      this.styleWidth = e.target.innerWidth
+      this.styleHeight = e.target.innerHeight
     }
   }
 }
