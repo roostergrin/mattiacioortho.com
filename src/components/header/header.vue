7294e5161aace6786907ee021e3dc78ca0ab8a6d
spragala <me@spragala.com>
2018-05-10 09:31:50 -0700

    adds dropdown and scroll effect

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -5,9 +5,22 @@ import HomeHero from 'components/home-hero/home-hero'
 import Menu from 'components/menu/menu'
 
 export default {
+  data () {
+    return {
+      scroll: false
+    }
+  },
   components: {
     HomeHero,
     Menu
+  },
+  methods: {
+    scrollOut () {
+      this.$store.dispatch('VIEW_NAV', true)
+    },
+    scrollIn () {
+      this.$store.dispatch('VIEW_NAV', false)
+    }
   }
 }
 </script>
