51a80881fb40251398eefc6d21919e8ead004c5e
spragala <me@spragala.com>
2018-05-15 12:53:02 -0700

    Switches API to migrated site

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -1,7 +1,7 @@
-const route = '//mattiacioortho.roostertest3.com/wp-json'
+const route = '//mattiacioortho.roostertest.com/wp-json'
 
 const api = () => {
-  return document.location.hostname === 'localhost' || document.location.href.indexOf('roostertest3') > -1 ? route : document.location.protocol + '/wp-json'
+  return document.location.hostname === 'localhost' || document.location.href.indexOf('roostertest') > -1 ? route : document.location.protocol + '/wp-json'
 }
 
 export default api()
