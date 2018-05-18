99936efc4ac85b6882b7c4c571e3f8eaa97e6f2b
spragala <me@spragala.com>
2018-05-18 13:58:40 -0700

    feat(new-pats): adds financial info section

--- src/pages/new-patients/new-patients.vue
+++ src/pages/new-patients/new-patients.vue
@@ -1,6 +1,8 @@
 <template lang='pug' src='./new-patients.pug'></template>
 
 <script>
+import Divider from 'components/divider/divider'
+import Financial from 'components/custom/custom-new-pats-financial/custom-new-pats-financial'
 import First from 'components/custom/custom-new-pats-first/custom-new-pats-first'
 import Loader from 'components/loader/loader'
 
@@ -15,6 +17,8 @@ export default {
     }
   },
   components: {
+    Divider,
+    Financial,
     First,
     Loader
   }
