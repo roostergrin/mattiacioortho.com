83364c59db6ca20c96a41ed66359f617bed96a46
spragala <me@spragala.com>
2018-06-15 14:25:30 -0700

    updates phone in footer

--- src/components/hero-mobile/hero-mobile.vue
+++ src/components/hero-mobile/hero-mobile.vue
@@ -1,6 +1,7 @@
 <template lang='pug' src='./hero-mobile.pug'></template>
 
 <script>
+import Icon from 'components/icon/icon'
 
 export default {
   data () {
@@ -8,14 +9,18 @@ export default {
       scroll: false
     }
   },
-  // components: {
-  // },
+  components: {
+    Icon
+  },
   methods: {
     scrollOut () {
       this.$store.dispatch('VIEW_NAV', true)
     },
     scrollIn () {
       this.$store.dispatch('VIEW_NAV', false)
+    },
+    makeAppt () {
+      this.$router.push('/contact-us#form')
     }
   }
 }
