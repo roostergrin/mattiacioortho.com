0758f8a2474f406a7fd8cdedba25a9002926f5cb
spragala <me@spragala.com>
2018-06-26 16:24:11 -0700

    runs build for tests

--- dist/functions/email.php
+++ dist/functions/email.php
@@ -12,6 +12,10 @@ function rg_register_routes () {
     'methods' => WP_REST_Server::CREATABLE,
     'callback' => 'rg_serve_route_sponsor'
   ));
+  register_rest_route('rg-mail/v1', 'family-fun', array(
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'rg_serve_route_family_fun'
+  ));
 }
 
 // function for handling post request to new api route
@@ -43,7 +47,7 @@ function rg_serve_route () {
 }
 
 // Request Sponsor Form
-function rg_serve_route () {
+function rg_serve_route_sponsor () {
   require('wp-load.php');
 
   $data = json_decode(file_get_contents("php://input"), true);
@@ -61,6 +65,35 @@ function rg_serve_route () {
   $message .= '<p><h4><strong>Date:</strong></h4> ' . $data['date'] . '</p>';
   $message .= '<p><h4><strong>Cause/Nature of org:</strong></h4> ' . $data['cause'] . '</p>';
   $message .= '<p><h4><strong>Website:</strong></h4> ' . $data['website'] . '</p>';
+  $message .= '</body></html>';
+
+  $sent_message = wp_mail($to, $subject, $message, $headers);
+
+  if ($sent_message) {
+    echo 'Email has been received!';
+  } else {
+    echo 'Could not send email.';
+  }
+}
+
+function rg_serve_route_family_fun () {
+  require('wp-load.php');
+
+  $data = json_decode(file_get_contents("php://input"), true);
+  // $from = 'info@wordpress.com,';
+  $to = 'matt.sprague@roostergrin.com';
+  $subject = 'Website Contact Form';
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+  $message = '<html><body>';
+  $message .= '<p><h4><strong>Form Submission by: </strong></h4>' . $data['fullName'] . '</p>';
+  $message .= '<p><h4><strong>Phone: </strong></h4>' . $data['patientName'] . '</p>';
+  $message .= '<p><h4><strong>Email: </strong></h4>' . $data['contactInfo'] . '</p>';
+  $message .= '<p><h4><strong>No. of Family Members:</strong></h4> ' . $data['eventName'] . '</p>';
+  $message .= '<p><h4><strong>Dates:</strong></h4> ' . $data['date'] . '</p>';
+  $message .= '<p><h4><strong>Guest Name:</strong></h4> ' . $data['cause'] . '</p>';
+  $message .= '<p><h4><strong>Guest Email:</strong></h4> ' . $data['website'] . '</p>';
   $message .= '</body></html>';
 
   $sent_message = wp_mail($to, $subject, $message, $headers);
