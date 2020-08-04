abd45fe8ac874bfa56ada5bf9939416071483688
David Ramirez <davidraviles@gmail.com>
2020-08-03 18:08:37 -0700

    request(covid-19-updates|custom-covid-19-updates)#38393: created 'covid-19 updates' page.

---
+++ src/components/custom/custom-covid-19-updates/custom-covid-19-updates.vue
@@ -0,0 +1,12 @@
+<template lang='pug' src='./custom-covid-19-updates.pug'></template>
+
+<script>
+export default {
+  props: ['props'],
+  computed: {
+    shape () {
+      return require(`@/assets/icons/Group.svg`)
+    }
+  }
+}
+</script>
---
+++ src/pages/covid-19-updates/covid-19-updates.vue
@@ -0,0 +1,24 @@
+<template lang='pug' src='./covid-19-updates.pug'></template>
+
+<script>
+import Divider from 'components/divider/divider'
+import Loader from 'components/loader/loader'
+import COVID19 from 'components/custom/custom-covid-19-updates/custom-covid-19-updates'
+
+export default {
+  name: 'covid-19 updates',
+  computed: {
+    props () {
+      return this.$store.state.pages['covid-19-updates']
+    },
+    loading () {
+      return this.$store.state.loading
+    }
+  },
+  components: {
+    Divider,
+    Loader,
+    COVID19
+  }
+}
+</script>
