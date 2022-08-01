222410217908179a4ae44d2028eabfc1a97ef1e3
Sebastian Park <me@sebastian-park.com>
2022-08-01 12:59:08 -0700

    requests(index.php) | added GTM script - ticket #62678

--- dist/index.php
+++ dist/index.php
@@ -24,6 +24,14 @@
       </noscript>
     <?php endif; ?>
     <!-- End Facebook Pixel Code -->
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-567L2M5');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body <?php body_class(); ?>>
   <div id="app"></div>
@@ -55,6 +63,12 @@
   <!-- Ruby Chat Code -->
   <script>window.rubyApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};(function(){var e="1c922a92-3c62-4e84-8b62-b93f46c3b681";var a=false;var t=document.createElement("script");t.async=true;t.type="text/javascript";t.src="https://chatwidget.ruby.com/"+e;document.getElementsByTagName("HEAD").item(0).appendChild(t);t.onreadystatechange=t.onload=function(t){if(!a&&(!this.readyState||this.readyState=="loaded"||this.readyState=="complete")){if(window.RubyChat)window.RubyChat({c:e});a=true}}})();</script>
   <!-- Ruby Chat Code -->
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-567L2M5"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
   <!-- Hotjar Tracking Code for https://mattiacioortho.com -->
   <script>
       (function(h,o,t,j,a,r){
