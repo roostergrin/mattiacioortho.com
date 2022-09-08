7fdbb5cb8bede198b76dc6d5a42b6dfa8987bfbd
phamiter253 <phamiter253@gmail.com>
2022-09-08 10:13:21 -0700

    requests(index.php) Adding GTM script #63978

--- dist/index.php
+++ dist/index.php
@@ -32,6 +32,14 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-567L2M5');</script> -->
     <!-- End Google Tag Manager -->
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-567L2M5');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body <?php body_class(); ?>>
   <div id="app"></div>
@@ -65,10 +73,9 @@
   <!-- Ruby Chat Code -->
 
   <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-567L2M5"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-567L2M5"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
-
   <!-- Hotjar Tracking Code for https://mattiacioortho.com -->
   <script>
       (function(h,o,t,j,a,r){
